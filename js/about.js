3a35ee1710808567126e6d16ae5267c1a2f136a0
Levv77 <oow_woo_@naver.com>
2025-05-21 17:16:41 +0900

    about 수정

--- js/about.js
+++ js/about.js
@@ -1,1 +1,12 @@
 const swiper = new Swiper('.swiper', { loop: true, autoplay: { delay: 3000 } });
+
+// from 시작하는 지점
+// const gsapFrom = gsap.from('.about-greet', {y: 0, duration: 2, ease: "bounce"});
+
+// from to 시작점과 끝지점 설정
+// const gsapFromTo = gsap.fromTo('.about-greet', {y: 500}, {duration: 2, y: 0});
+
+// to 끝나는 지점
+// const gsapTo = gsap.to('.about-greet', {y: 500, duration: 2, ease: "bounce"});
+
+const gsapFromTo = gsap.fromTo('.about-greet', { y: 0 }, { y: 400, duration: 3, ease: "bounce"  });
